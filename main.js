e112db7ea3fa3686219460fb63702f8fa26c7375
unknown <ezekuiel100@gmail.com>
2024-01-30 22:37:38 -0300

    Parser

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function tokenizer(input) {
 
     if (char === "(") {
       tokens.push({
-        type: "paran",
+        type: "paren",
         value: "(",
       });
 
@@ -17,7 +17,7 @@ function tokenizer(input) {
 
     if (char === ")") {
       tokens.push({
-        type: "paran",
+        type: "paren",
         value: ")",
       });
 
@@ -73,10 +73,70 @@ function tokenizer(input) {
       continue;
     }
 
-    throw new console.error("I dont know what this character is: " + char);
+    throw new Error("I dont know what this character is: " + char);
   }
 
   return tokens;
 }
 
-console.log(tokenizer("(add 2 (subtract 4 2))"));
+let tokens = tokenizer("(add 2 (subtract 4 2))");
+
+function parser(tokens) {
+  let current = 0;
+
+  function walk() {
+    let token = tokens[current];
+
+    if (token.type === "number") {
+      current++;
+
+      return {
+        type: "NumberLiteral",
+        value: token.value,
+      };
+    }
+
+    if (token.type === "string") {
+      current++;
+
+      return {
+        type: "StringLiteral",
+        value: token.value,
+      };
+    }
+
+    if (token.type === "paren" && token.value === "(") {
+      token = tokens[++current];
+
+      let node = {
+        type: "CallExpression",
+        name: token.value,
+        params: [],
+      };
+
+      token = tokens[++current];
+
+      while (
+        token.type !== "paren" ||
+        (token.type === "paren" && token.value !== ")")
+      ) {
+        node.params.push(walk());
+        token = tokens[current];
+      }
+
+      current++;
+      return node;
+    }
+
+    throw new Error(token.type);
+  }
+
+  let ast = { type: "Program", body: [] };
+
+  while (current < tokens.length) {
+    ast.body.push(walk());
+  }
+
+  return ast;
+}
+parser(tokens);
